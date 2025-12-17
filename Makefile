ac027510dbd2058a7deae95cee3ef33f007802a5
Thibault Malpertu <tmalpert@z2r4p8.42lyon.fr>
2025-12-17 10:14:07 +0100

    All my mistakes have been corrected.

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ DEPS := $(patsubst %.c,$(BUILDDIR)/%.d,$(SRCS))
 
 ARFLAGS := rcs
 DEPS_FLAGS := -MP -MD -MF
-CFLAGS := -Wall -Wextra -Werror -g
+CFLAGS := -Wall -Wextra -Werror
 
 all: $(NAME)
 
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tmalpert <tmalpert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 15:53:49 by tmalpert          #+#    #+#             */
-/*   Updated: 2025/12/15 14:01:54 by tmalpert         ###   ########.fr       */
+/*   Updated: 2025/12/17 10:04:10 by tmalpert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <stdlib.h>
-# include <stdio.h>
 # include <unistd.h>
-# include <stdint.h>
 # include <stdbool.h>
 
 typedef struct s_attribution
@@ -32,7 +29,6 @@ typedef struct s_attribution
 int		ft_printf(const char *tab, ...);
 int		ft_putchar(char c);
 int		ft_putstr(char *str);
-char	*ft_itoa(int n);
 int		ft_putnbr(int nb, int cap);
 int		ft_putnbr_u(unsigned int nb, unsigned int base, int cap);
 int		ft_adress(void *p);
